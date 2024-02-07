f42323199c7dc4aa8c458d862b1199587a9294d8
Sergey Usachev <kperep@gmail.com>
2024-02-07 17:32:45 +0700

    work in progress

--- main.py
+++ main.py
@@ -7,13 +7,35 @@ url = "https://kitsu.io/api/edge/anime?filter[text]="
 
 def anime_info():
     def anime_search(event):
+        def show_info():
+            search = selected_item.get()
+            select_anime = 0
+            for i in range(size):
+                if data["data"][i]["attributes"]["canonicalTitle"] == search:
+                    select_anime = i
+            for i in buttons:
+                i.destroy()
+
+
+
         query = search_entry.get()
         raw_data = requests.get(url + query)
         data = raw_data.json()
         # вывод результатов
-        size = len(data["data"])
-        for i in range(size):
-            results.insert(tk.END, str(i + 1) + " " + data["data"][i]["attributes"]["canonicalTitle"] + "\n")
+        size = len(data["data"])        # кол-во аниме в выдаче
+        show = [data["data"][i]["attributes"]["canonicalTitle"] for i in range(size)]   # поисковая выдача
+        delta = 0
+        selected_item = tk.StringVar()
+        buttons = []
+        for item in show:
+            item_btn = tk.Radiobutton(text=item, value=item, variable=selected_item)
+            item_btn.place(x=75, y=150+delta)
+            buttons.append(item_btn)
+            delta += 30
+        result_btn = tk.Button(root, text="Выдать информацию", command=show_info)
+        result_btn.place(x=300, y=500)
+
+
 
     root = tk.Tk()
     root.title("Anime Info")
@@ -25,8 +47,6 @@ def anime_info():
     search_entry.place(x=75, y=80)
     search_button = tk.Button(root, text="Как-то так...", font=("Comic Sans MS", 15))
     search_button.place(x=425, y=67)
-    results = tk.Text(root, width=80, height=25)
-    results.place(x=25, y=150)
     search_button.bind("<Button-1>", anime_search)
     root.mainloop()
 
