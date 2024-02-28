5174295de3d6f130ee015dff9675b60183b32b43
Sergey Usachev <kperep@gmail.com>
2024-02-28 17:37:14 +0700

    adding info

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ url = "https://kitsu.io/api/edge/anime?filter[text]="
 def anime_info():
     def anime_search(event):
         def show_info():
+            def text_wrap(text, size):
+                all_text = list(text.split("\n"))
+                result = ""
+                for elem in all_text:
+                    for i in range(len(elem) // size):
+                        if elem[0] == " ":
+                            result += elem[1:size]
+                        else:
+                            result += elem[:size]
+                        counter = 0
+                        while (size + counter < len(elem) - 1) and (elem[size + counter] != " "):
+                            result += elem[size + counter]
+                            counter += 1
+                        result += "\n"
+                        elem = elem[size + counter:]
+                    result += elem + "\n"
+                return result
+
             global poster
             search = selected_item.get()
             select_anime = 0
@@ -29,6 +47,10 @@ def anime_info():
             poster_label.place(x=25, y=125)
             for i in buttons:
                 i.destroy()
+            anime_title = tk.Label(root, text=text_wrap(data["data"][select_anime]["attributes"]["canonicalTitle"], 30), font=("Comic Sans MS", 20), justify=tk.LEFT)
+            anime_title.place(x=350, y=130)
+            anime_synopsis = tk.Label(root, text=text_wrap(data["data"][select_anime]["attributes"]["synopsis"], 55), font=("Comic Sans MS", 12), justify=tk.LEFT)
+            anime_synopsis.place(x=325, y=250)
 
         query = search_entry.get()
         raw_data = requests.get(url + query)
@@ -48,10 +70,9 @@ def anime_info():
         result_btn = tk.Button(root, text="Выдать информацию", command=show_info)
         result_btn.place(x=300, y=500)
 
-
     root = tk.Tk()
     root.title("Anime Info")
-    root.geometry("700x700")
+    root.geometry("800x800")
     # интерфейс
     main_label = tk.Label(root, text="Anime Info", font=("Comic Sans MS", 20))
     main_label.place(x=275, y=10)
